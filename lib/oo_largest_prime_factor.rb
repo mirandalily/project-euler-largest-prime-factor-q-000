3cca1ed0602037479e0c13c0410f47d6899a3ec9
mirandalily <mirandaraymond@me.com>
2017-05-24 16:47:22 -0700

    setting up class

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,10 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  def initialize(input)
+    @number = largest_prime_factor(input)
+  end
+
+  def largest_prime_factor(input)
+  end
+end
