9243842cd63a9b1569a485afce1bfecc4dd62752
mirandalily <mirandaraymond@me.com>
2017-05-29 12:13:11 -0700

    tests not passing yet

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+def largest_prime_factor(input)
+  prime_number = input
+  (2..Math.sqrt(input).to_i).each do |i|
+    return false if i <= 1
+    prime_number = prime_number / i while (prime > i && prime_number % i == 0)
+  end
+  prime_number
+end
